0f6a75d422b298946541fcefadf378795e5f2934
fireph <fireph@users.noreply.github.com>
2014-11-12 20:08:17 -0800

    basic server stuff to print updates

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var lineData = [];
+
 app.get('/', function(req, res){
   res.send('index');
 });
@@ -9,8 +11,19 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  socket.on('test', function(data) {
+  socket.on('wipe', function(data) {
+    console.log('wipe');
+    lineData = [];
+  });
+
+  socket.on('lineTo', function(data) {
     console.log(data);
+    lineData.push(data);
+  });
+
+  socket.on('moveTo', function(data) {
+    console.log(data);
+    lineData.push(data);
   });
 });
 
