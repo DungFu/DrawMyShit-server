cf454775a8639ca5cce29e2abdf89326054941f0
fireph <fireph@users.noreply.github.com>
2014-11-13 00:11:08 -0800

    basic app changes and fixes for heroku

--- app.js
+++ app.js
@@ -4,32 +4,35 @@ var io = require('socket.io')(http);
 
 var lineData = [];
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.send('index');
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', function(socket) {
+  for (var i = 0; i < lineData.length; i++) {
+    socket.emit(lineData[i][0], lineData[i][1]);
+  }
 
-  socket.on('wipe', function(data) {
-    console.log('wipe');
+  socket.on('erase', function(data) {
     lineData = [];
-    socket.broadcast.emit('wipe');
+    socket.broadcast.emit('erase');
   });
 
   socket.on('lineTo', function(data) {
-    console.log(data);
-    lineData.push(data);
+    data.id = socket.id;
+    lineData.push(['lineTo', data]);
     socket.broadcast.emit('lineTo', data);
   });
 
   socket.on('moveTo', function(data) {
-    console.log(data);
-    lineData.push(data);
+    data.id = socket.id;
+    lineData.push(['moveTo', data]);
     socket.broadcast.emit('moveTo', data);
   });
 });
 
-http.listen(9001, function(){
-  console.log('listening on *:9001');
+var port = process.env.PORT || 3000;
+
+http.listen(port, function() {
+  console.log('listening on port: '+port);
 });
