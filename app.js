d786b804d504126673f51756ff1603ae4b7076b2
fireph <fireph@users.noreply.github.com>
2015-06-16 22:43:37 -0700

    changed to heroku redis

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var io = require('socket.io')(http);
 var redis = require('redis');
 var url = require('url');
 
+// Set up Heroku Redis
 var client;
 if (process.env.REDISCLOUD_URL) {
-  var redisURL = url.parse(process.env.REDISCLOUD_URL);
-  client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+  var redisURL = url.parse(process.env.REDIS_URL);
+  client = redis.createClient(redisURL.port, redisURL.hostname);
   client.auth(redisURL.auth.split(":")[1]);
 } else {
   client = redis.createClient();
